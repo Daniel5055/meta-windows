a90da0305919183cf021ca6970a61d1f1f710311
Daniel Brathagen <dbrathagen@gmail.com>
2023-02-02 18:27:48 +0100

    Applied prettier formatting

--- backend/index.js
+++ backend/index.js
@@ -2,25 +2,25 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
+const { Server } = require('socket.io');
 const io = new Server(server);
-const port = 3000
+const port = 3000;
 
-app.use(express.static('frontend'))
+app.use(express.static('frontend'));
 
 io.on('connection', (socket) => {
-  console.log(socket.id, 'connected')
+  console.log(socket.id, 'connected');
 
   socket.on('state', (state) => {
     socket.broadcast.emit('state', { id: socket.id, state });
-  })
+  });
 
   socket.on('disconnect', () => {
-  console.log(socket.id, 'disconnected')
-    socket.broadcast.emit('delete', { id: socket.id })
-  })
-})
+    console.log(socket.id, 'disconnected');
+    socket.broadcast.emit('delete', { id: socket.id });
+  });
+});
 
 server.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- frontend/script.js
+++ frontend/script.js
@@ -4,56 +4,67 @@ const myself = { state: {} };
 const server = io();
 updateSelf();
 
-myself.state = { x: window.screenX, y: window.screenY, w: window.innerWidth, h: window.innerHeight };
+myself.state = {
+  x: window.screenX,
+  y: window.screenY,
+  w: window.innerWidth,
+  h: window.innerHeight,
+};
 
 const colors = [
-    'rgba(255, 0, 0, 0.5)',
-    'rgba(0, 255, 0, 0.5)',
-    'rgba(0, 0, 255, 0.5)',
-]
+  'rgba(255, 0, 0, 0.5)',
+  'rgba(0, 255, 0, 0.5)',
+  'rgba(0, 0, 255, 0.5)',
+];
 
 function updateSelf() {
-    myself.state = { x: window.screenX, y: window.screenY, w: window.innerWidth, h: window.innerHeight };
-    server.emit('state', myself.state);
-    Object.values(windows).forEach((w) => update(w))
+  myself.state = {
+    x: window.screenX,
+    y: window.screenY,
+    w: window.innerWidth,
+    h: window.innerHeight,
+  };
+  server.emit('state', myself.state);
+  Object.values(windows).forEach((w) => update(w));
 }
 
-
 server.on('state', (msg) => {
-    if (!(msg.id in windows)) {
-        windows[msg.id] = {
-            state: msg.state,
-            el: document.createElement('div'),
-        }
-        document.body.append(windows[msg.id].el);
-        windows[msg.id].el.style.backgroundColor = colors[Object.values(windows).length - 1]
-        console.log(Object.values(windows).length, colors[0])
-    } else {
-        windows[msg.id].state = msg.state;
-    }
+  if (!(msg.id in windows)) {
+    windows[msg.id] = {
+      state: msg.state,
+      el: document.createElement('div'),
+    };
+    document.body.append(windows[msg.id].el);
+    windows[msg.id].el.style.backgroundColor =
+      colors[Object.values(windows).length - 1];
+    console.log(Object.values(windows).length, colors[0]);
+  } else {
+    windows[msg.id].state = msg.state;
+  }
 
-    update(windows[msg.id])
-})
+  update(windows[msg.id]);
+});
 
 function update(other) {
-    if (!other) return;
+  if (!other) return;
 
+  const target = other.el;
 
-    const target = other.el;
+  const left = Math.max(other.state.x - myself.state.x, 0);
+  const top = Math.max(other.state.y - myself.state.y, 0);
+  const right = Math.max(
+    myself.state.x + myself.state.w - other.state.x - other.state.w,
+    0
+  );
+  const bottom = Math.max(
+    myself.state.y + myself.state.h - other.state.y - other.state.h,
+    0
+  );
 
-    const left = Math.max(other.state.x - myself.state.x, 0);
-    const top = Math.max(other.state.y - myself.state.y, 0);
-    const right = Math.max(myself.state.x + myself.state.w - other.state.x - other.state.w, 0);
-    const bottom = Math.max(myself.state.y + myself.state.h - other.state.y - other.state.h, 0);
-
-    console.log('hello', left, right, myself.state.w, other)
-
-    target.style.left = `${left}px`;
-    target.style.top = `${top}px`;
-    target.style.width = `${Math.max(myself.state.w - left - right + 1, 0)}px`;
-    target.style.height = `${Math.max(myself.state.h - top - bottom + 1, 0)}px`;
-
-    console.log('hi', target.style.left, target.style.top, target.style.width, target.style.height)
+  target.style.left = `${left}px`;
+  target.style.top = `${top}px`;
+  target.style.width = `${Math.max(myself.state.w - left - right + 1, 0)}px`;
+  target.style.height = `${Math.max(myself.state.h - top - bottom + 1, 0)}px`;
 }
 
 // Movement detection
@@ -61,14 +72,14 @@ let oldX = window.screenX;
 let oldY = window.screenY;
 
 let interval = setInterval(() => {
-    if (window.screenX !== oldX || window.screenY !== oldY) {
-        updateSelf();
-        oldX = window.screenX;
-        oldY = window.screenY;
-    }
-}, 0)
+  if (window.screenX !== oldX || window.screenY !== oldY) {
+    updateSelf();
+    oldX = window.screenX;
+    oldY = window.screenY;
+  }
+}, 0);
 
 // Size detection
 window.addEventListener('resize', (e) => {
-    updateSelf();
-})
+  updateSelf();
+});
