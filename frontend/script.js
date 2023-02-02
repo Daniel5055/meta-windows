2255d41fb9097b259795e9932fe39ddc69fee54b
Daniel Brathagen <dbrathagen@gmail.com>
2023-02-02 18:19:34 +0100

    Touching up a bit Fixed some minor bugs Support for more than 2 windows interacting

--- frontend/script.js
+++ frontend/script.js
@@ -2,15 +2,22 @@ const windows = {};
 const myself = { state: {} };
 
 const server = io();
-    myself.state = { x: window.screenX, y: window.screenY, w: window.innerWidth, h: window.innerHeight };
+updateSelf();
+
+myself.state = { x: window.screenX, y: window.screenY, w: window.innerWidth, h: window.innerHeight };
+
+const colors = [
+    'rgba(255, 0, 0, 0.5)',
+    'rgba(0, 255, 0, 0.5)',
+    'rgba(0, 0, 255, 0.5)',
+]
 
 function updateSelf() {
     myself.state = { x: window.screenX, y: window.screenY, w: window.innerWidth, h: window.innerHeight };
     server.emit('state', myself.state);
-    update();
+    Object.values(windows).forEach((w) => update(w))
 }
 
-updateSelf();
 
 server.on('state', (msg) => {
     if (!(msg.id in windows)) {
@@ -18,30 +25,19 @@ server.on('state', (msg) => {
             state: msg.state,
             el: document.createElement('div'),
         }
-        document.body.append(windows[msg.id].el)
+        document.body.append(windows[msg.id].el);
+        windows[msg.id].el.style.backgroundColor = colors[Object.values(windows).length - 1]
+        console.log(Object.values(windows).length, colors[0])
     } else {
         windows[msg.id].state = msg.state;
     }
 
-    console.log(windows);
-    const other = windows[msg.id];
-
-    const target = other.el;
-
-    const left = Math.max(other.state.x - myself.state.x, 0);
-    const top = Math.max(other.state.y - myself.state.y, 0);
-    const right = Math.max(myself.state.x + myself.state.w - other.state.x - other.state.w, 0);
-    const bottom = Math.max(myself.state.y + myself.state.h - other.state.y - other.state.h, 0);
-
-    target.style.left = `${left}px`;
-    target.style.top = `${top}px`;
-    target.style.width = `${myself.state.w - left - right}px`;
-    target.style.height = `${myself.state.h - top - bottom}px`;
+    update(windows[msg.id])
 })
 
-function update() {
-    const other = Object.values(windows)[0];
+function update(other) {
     if (!other) return;
+
 
     const target = other.el;
 
@@ -50,10 +46,14 @@ function update() {
     const right = Math.max(myself.state.x + myself.state.w - other.state.x - other.state.w, 0);
     const bottom = Math.max(myself.state.y + myself.state.h - other.state.y - other.state.h, 0);
 
+    console.log('hello', left, right, myself.state.w, other)
+
     target.style.left = `${left}px`;
     target.style.top = `${top}px`;
-    target.style.width = `${myself.state.w - left - right}px`;
-    target.style.height = `${myself.state.h - top - bottom}px`;
+    target.style.width = `${Math.max(myself.state.w - left - right + 1, 0)}px`;
+    target.style.height = `${Math.max(myself.state.h - top - bottom + 1, 0)}px`;
+
+    console.log('hi', target.style.left, target.style.top, target.style.width, target.style.height)
 }
 
 // Movement detection
